c23d090824573c7850959636f6332e6735ab4cf2
unknown <alwyyourbae@gmail.com>
2023-08-26 07:46:15 -0700

    add some effect

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
 
+import { useState } from "react";
+
 export default function Navbar() {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (tabName: number) => {
+    setActiveTab(tabName);
+  };
   return (
     <div className="absolute right-5">
       <button className="sm:hidden relative group right-[-40px]">
@@ -12,19 +19,49 @@ export default function Navbar() {
       </button>
       <ul className="sm:flex text-right mt-2 italic text-sm sm:mr-4 sm:space-x-6">
         <li>
-          <Link to="/">Home</Link>
+          <Link
+            to="/"
+            className={activeTab === 0 ? "font-medium text-prim-blue underline underline-offset-8" : ""}
+            onClick={() => handleTabClick(0)}
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link
+            to="/about"
+            className={activeTab === 1 ? "font-medium text-prim-blue underline underline-offset-8" : ""}
+            onClick={() => handleTabClick(1)}
+          >
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/projects">Projects</Link>
+          <Link
+            to="/projects"
+            className={activeTab === 2 ? "font-medium text-prim-blue underline underline-offset-8" : ""}
+            onClick={() => handleTabClick(2)}
+          >
+            Projects
+          </Link>
         </li>
         <li>
-          <Link to="/skills">Skills</Link>
+          <Link
+            to="/skills"
+            className={activeTab === 3 ? "font-medium text-prim-blue underline underline-offset-8" : ""}
+            onClick={() => handleTabClick(3)}
+          >
+            Skills
+          </Link>
         </li>
         <li>
-          <Link to="/contacts">Contacts</Link>
+          <Link
+            to="/contacts"
+            className={activeTab === 4 ? "font-medium text-prim-blue underline underline-offset-8" : ""}
+            onClick={() => handleTabClick(4)}
+          >
+            Contacts
+          </Link>
         </li>
       </ul>
     </div>
--- src/page/skills/Skills.tsx
+++ src/page/skills/Skills.tsx
@@ -1,6 +1,5 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-//import 'react-tabs/style/react-tabs.css';
-//import NavSkill from "../../component/navbar/NavSkill";
+import { useState } from "react";
 import {
   language,
   framework,
@@ -10,17 +9,32 @@ import {
 } from "../../component/card/Card";
 
 export default function Skill() {
+  const [activeTab, setActiveTab] = useState(0); // State awal diatur ke indeks 0
   return (
     <div>
       <h1 className="font-semibold text-base sm:text-2xl relative text-center top-40 sm:top-20">
         My Skills
       </h1>
-      <Tabs className={`relative top-40 sm:top-24`}>
-        <TabList className={`flex text-right mt-2 font-medium text-sm sm:mr-4 justify-center space-x-6`}>
-          <Tab>Language</Tab>
-          <Tab>Framework</Tab>
-          <Tab>Library</Tab>
-          <Tab>DBMS</Tab>
+      <Tabs
+        selectedIndex={activeTab} // Tentukan indeks tab yang aktif
+        onSelect={(index) => setActiveTab(index)} // Fungsi untuk mengubah indeks tab aktif
+        className={`relative top-40 sm:top-24`}
+      >
+        <TabList
+          className={`flex text-right mt-2 font-medium text-sm sm:mr-4 justify-center space-x-8`}
+        >
+          <Tab className={`px-1 rounded ${activeTab === 0 ? "text-prim-blue bg-sec-gray" : ""}`}>
+            Language
+          </Tab>
+          <Tab className={`px-1 rounded ${activeTab === 1 ? "text-prim-blue bg-sec-gray" : ""}`}>
+            Framework
+          </Tab>
+          <Tab className={`px-1 rounded ${activeTab === 2 ? "text-prim-blue bg-sec-gray" : ""}`}>
+            Library
+          </Tab>
+          <Tab className={`px-1 rounded ${activeTab === 3 ? "text-prim-blue bg-sec-gray" : ""}`}>
+            DBMS
+          </Tab>
         </TabList>
 
         <TabPanel>
