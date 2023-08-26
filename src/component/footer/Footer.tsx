9597e7abbbb8656ef29d734185aca4af06576aab
unknown <alwyyourbae@gmail.com>
2023-08-26 15:25:28 -0700

    update footer

--- src/component/footer/Footer.tsx
+++ src/component/footer/Footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
   return (
-    <div className="text-center relative text-xs bottom-[-43vh] sm:bottom-[-59vh] pb-2 text-prim-gray">© 2023 - present Alwi Wahyu Waludin. <br />All rights reserved.</div>
+    <div className="text-center relative text-xs bottom-[-43vh] sm:bottom-[-59vh] pb-2 text-prim-gray">© 2023 - present <span className="text-prim-black">Alwi Wahyu Waludin.</span> <br />All rights reserved.</div>
   )
 }
