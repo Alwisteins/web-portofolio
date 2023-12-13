1886082bf5f9e81f36ce8760317ab73af25d2d91
unknown <alwyyourbae@gmail.com>
2023-12-13 06:27:01 -0800

    change imperative code to declarative code

--- src/page/contacts/Contacts.tsx
+++ src/page/contacts/Contacts.tsx
@@ -1,6 +1,29 @@
 import MyForm from "../../component/form/Form";
 
 export default function Contacts() {
+  const contacts: {
+    image: string;
+    anchor: string;
+    name: string;
+  }[] = [
+    { image: "/mail.png", anchor: "", name: "alwyyourbae@gmail.com" },
+    {
+      image: "/bxl-github.svg",
+      anchor: "https://github.com/Alwisteins",
+      name: "Alwisteins",
+    },
+    {
+      image: "/bxl-linkedin.svg",
+      anchor: "https://www.linkedin.com/in/alwi-wahyu-8bb256288/",
+      name: "Alwisteins",
+    },
+    {
+      image: "/instagram.png",
+      anchor:
+        "https://instagram.com/alwywhyuu_?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
+      name: "alwywhyuu_",
+    },
+  ];
   return (
     <div className="z-[-100]">
       <h1 className="font-semibold text-base sm:text-2xl relative text-center sm:top-20">
@@ -12,31 +35,17 @@ export default function Contacts() {
         my social media below:
       </p>
       <div className="grid grid-cols-2 gap-4 w-[100px] ml-24 sm:mx-auto relative top-8 sm:top-28">
-        <img src="/mail.png" alt="" className="w-6 sm:w-8 h-6 sm:h-8" />
-        <a className="underline text-xs self-center sm:text-base" href="">
-          alwyyourbae@gmail.com
-        </a>
-        <img src="/bxl-github.svg" alt="" className="w-6 sm:w-8 h-6 sm:h-8" />
-        <a
-          className="underline text-xs self-center sm:text-base"
-          href="https://github.com/Alwisteins"
-        >
-          Alwisteins
-        </a>
-        <img src="/bxl-linkedin.svg" alt="" className="w-6 sm:w-8 h-6 sm:h-8" />
-        <a
-          className="underline text-xs self-center sm:text-base"
-          href="https://www.linkedin.com/in/alwi-wahyu-8bb256288/"
-        >
-          Alwisteins
-        </a>
-        <img src="/instagram.png" alt="" className="w-6 sm:w-8 h-6 sm:h-8" />
-        <a
-          className="underline text-xs self-center sm:text-base"
-          href="https://instagram.com/alwywhyuu_?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D"
-        >
-          @alwywhyuu_
-        </a>
+        {contacts.map((item) => (
+          <>
+            <img src={item.image} alt="" className="w-6 sm:w-8 h-6 sm:h-8" />
+            <a
+              className="underline text-xs self-center sm:text-base"
+              href={item.anchor}
+            >
+              {item.name}
+            </a>
+          </>
+        ))}
       </div>
       <MyForm />
     </div>
