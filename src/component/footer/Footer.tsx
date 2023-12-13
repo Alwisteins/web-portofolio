659f9507ba0f7a2bec2bc52cf6625fb16a3b50bc
unknown <alwyyourbae@gmail.com>
2023-12-13 05:50:00 -0800

    formating lint

--- src/component/footer/Footer.tsx
+++ src/component/footer/Footer.tsx
@@ -1,5 +1,9 @@
 export default function Footer() {
   return (
-    <div className="text-center relative text-lg bottom-[-43vh] sm:bottom-[-59vh] pb-2 text-prim-gray">© 2023 - present <span className="text-prim-black">Alwi Wahyu Waludin.</span> <br />All rights reserved.</div>
-  )
+    <div className="text-center relative text-lg bottom-[-43vh] sm:bottom-[-59vh] pb-2 text-prim-gray">
+      © 2023 - present{" "}
+      <span className="text-prim-black">Alwi Wahyu Waludin.</span> <br />
+      All rights reserved.
+    </div>
+  );
 }
