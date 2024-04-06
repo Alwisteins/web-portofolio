eaaccc3cab06a758aed6019d34df6e6fba6551dd
unknown <alwyyourbae@gmail.com>
2024-04-06 14:26:13 +0700

    add animation

--- src/section/home/Home.tsx
+++ src/section/home/Home.tsx
@@ -1,12 +1,39 @@
 import Image from "next/image";
 import AlwiProfilePicture from "../../../public/alwi-django.jpeg";
+import Texture from "../../../public/texture.webp";
 import { HiDocumentDownload } from "react-icons/hi";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { Button } from "flowbite-react";
 
 export default function Home() {
   return (
-    <div className="flex flex-row-reverse justify-evenly items-center h-[90vh] w-screen bg-[#f9f9f9]">
+    <div className="flex justify-evenly items-center h-[90vh] w-screen bg-[#f9f9f9]">
+      <div className="absolute top-[30%] left-[15%] w-full max-w-lg">
+        <div className="absolute top-0 left-8 w-60 h-60 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl animate-blob"></div>
+        <div className="absolute top-0 right-8 w-60 h-60 bg-teal-300 rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-4"></div>
+        <div className="absolute top-8 left-32 w-60 h-60 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-6"></div>
+      </div>
+      <div className="space-y-4 w-5/12 z-10">
+        <h1 className="text-xl sm:text-7xl text-prim-black font-bold">
+          Hi<span className="animate-wave inline-block p-1">👋</span>, im Alwi
+          Wahyu Waludin.
+        </h1>
+        <p className="text-xs sm:text-lg pl-2 text-slate-600">
+          A Fullstack Web Developer based in Pemalang, Indonesia.
+        </p>
+        <div className="ml-2 flex items-center space-x-3">
+          <Button color="blue">
+            <HiDocumentDownload className="mr-1 h-5 w-5" />
+            Download CV
+          </Button>
+          <a href="https://github.com/Alwisteins">
+            <FaGithub className="w-7 h-7" />
+          </a>
+          <a href="https://www.linkedin.com/in/alwi-wahyu-8bb256288/">
+            <FaLinkedin className="w-7 h-7" />
+          </a>
+        </div>
+      </div>
       <Image
         src={AlwiProfilePicture}
         alt="Alwi Main Image"
@@ -16,27 +43,6 @@ export default function Home() {
         quality={100}
         priority
       />
-      <div className="space-y-4 w-5/12">
-        <h1 className="text-xl sm:text-7xl text-prim-black font-bold">
-          Hi<span className="animate-wave inline-block p-1">👋</span>, im Alwi
-          Wahyu Waludin.
-        </h1>
-        <p className="text-xs sm:text-lg pl-2 text-slate-600">
-          A Fullstack Web Developer based in Pemalang, Indonesia.
-        </p>
-        <div className="ml-2 flex items-center space-x-3">
-        <Button color="blue">
-          <HiDocumentDownload className="mr-1 h-5 w-5" />
-          Download CV
-        </Button>
-        <a href="https://github.com/Alwisteins">
-          <FaGithub className="w-7 h-7" />
-        </a>
-        <a href="https://www.linkedin.com/in/alwi-wahyu-8bb256288/">
-          <FaLinkedin className="w-7 h-7" />
-        </a>
-        </div>
-      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/section/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/section/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -14,6 +14,25 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        blob: "blob 9s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+        },
       },
     },
   },
