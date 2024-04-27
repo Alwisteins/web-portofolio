a90c6043af37ac804e02af33925b219ff447a1e0
unknown <alwyyourbae@gmail.com>
2024-04-27 15:26:36 +0700

    remove animate, add mobile view

--- src/section/home/Home.tsx
+++ src/section/home/Home.tsx
@@ -1,23 +1,35 @@
 import Image from "next/image";
 import AlwiProfilePicture from "../../../public/alwi-django.jpeg";
-import BlobAnimate from "@/animate/BlobAnimate";
 import { HiDocumentDownload } from "react-icons/hi";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { Button } from "flowbite-react";
 
 export default function Home() {
   return (
-    <div className="flex justify-evenly items-center h-[90vh] w-screen bg-[#f9f9f9]">
-      <BlobAnimate />
-      <div className="space-y-4 w-5/12 z-10">
-        <h1 className="text-xl sm:text-7xl text-prim-black font-bold">
+    <div className="flex flex-col sm:flex-row-reverse items-center justify-evenly py-5 w-screen h-[90vh] bg-[#f9f9f9]">
+      <div className="flex sm:w-[22rem] sm:h-[22rem]">
+        <Image
+          src={AlwiProfilePicture}
+          alt="Alwi Main Image"
+          quality={100}
+          sizes="100vw"
+          priority
+          style={{ width: "100%", height: "auto" }}
+          className="border-solid border-black border-4 rounded-full"
+        />
+      </div>
+
+      <div className="flex flex-col items-center sm:items-start sm:w-[35rem] space-y-2">
+        <h1 className="text-2xl sm:text-7xl font-bold">
           Hi<span className="animate-wave inline-block p-1">👋</span>, im Alwi
           Wahyu Waludin.
         </h1>
-        <p className="text-xs sm:text-lg pl-2 text-slate-600">
+
+        <p className="text-xs sm:text-lg text-slate-600">
           A Fullstack Web Developer based in Pemalang, Indonesia.
         </p>
-        <div className="ml-2 flex items-center space-x-3">
+
+        <div className="flex items-center space-x-3">
           <Button color="blue">
             <HiDocumentDownload className="mr-1 h-5 w-5" />
             Download CV
@@ -30,15 +42,6 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <Image
-        src={AlwiProfilePicture}
-        alt="Alwi Main Image"
-        className="border-solid border-black border-4 rounded-full"
-        width={350}
-        height={350}
-        quality={100}
-        priority
-      />
     </div>
   );
 }
