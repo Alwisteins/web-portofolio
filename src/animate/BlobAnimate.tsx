6379682dce100198266f8ffa87db999f67f2e036
unknown <alwyyourbae@gmail.com>
2024-04-28 22:30:48 +0700

    apply texture, update position

--- src/animate/BlobAnimate.tsx
+++ src/animate/BlobAnimate.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Image from "next/image";
+import Texture from "@/texture.webp";
 
 export default function BlobAnimate() {
   return (
-    <div className="absolute top-[30%] left-[15%] w-full max-w-lg">
-      <div className="absolute top-0 left-8 w-60 h-60 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl animate-blob"></div>
-      <div className="absolute top-0 right-8 w-60 h-60 bg-teal-300 rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-4"></div>
-      <div className="absolute top-8 left-32 w-60 h-60 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-6"></div>
+    <div className="absolute sm:top-52 sm:left-0">
+      <div className="absolute z-20 w-[40rem] h-[40rem] -top-16 opacity-85 mix-blend-overlay">
+        <Image src={Texture} alt="Gradient Texture" className="w-full" />
+      </div>
+      <div className="absolute left-14 w-52 h-52 z-10 bg-blue-500 opacity-75 rounded-full filter blur-xl animate-blob"></div>
+      <div className="absolute top-4 left-32 w-80 h-80 bg-purple-400 rounded-full filter blur-xl animate-blob animation-delay-6"></div>
     </div>
   );
 }
