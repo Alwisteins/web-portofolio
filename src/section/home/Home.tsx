0ee6c3717a6f1a86801e225985e3dcd896150fac
unknown <alwyyourbae@gmail.com>
2024-05-04 23:14:42 +0700

    fix animate mobile view

--- src/section/home/Home.tsx
+++ src/section/home/Home.tsx
@@ -20,12 +20,12 @@ export default function Home() {
         />
       </div>
 
-      <div className="absolute sm:top-40 sm:left-0 left-10 top-80">
-        <div className="absolute z-20 w-[40rem] h-[40rem] sm:-top-16 opacity-60 mix-blend-overlay">
+      <div className="absolute sm:top-40 left-0 top-80">
+        <div className="absolute left-0 sm:-top-16 z-20 w-96 h-96 sm:w-[40rem] sm:h-[40rem] opacity-80 mix-blend-overlay">
           <Image src={Texture} alt="Gradient Texture" className="w-full" />
         </div>
-        <div className="absolute sm:left-14 w-52 h-52 z-10 bg-blue-500 opacity-60 rounded-full filter blur-2xl animate-blob"></div>
-        <div className="absolute sm:top-4 sm:left-32 w-72 h-72 bg-purple-400 opacity-75 rounded-full filter blur-2xl animate-blob animation-delay-6"></div>
+        <div className="absolute top-8 left-14 w-32 h-32 sm:w-52 sm:h-52 z-10 bg-blue-500 opacity-40 rounded-full filter blur-2xl animate-blob"></div>
+        <div className="absolute top-20 left-28 sm:left-32 w-52 h-52 sm:w-72 sm:h-72 bg-purple-400 opacity-100 rounded-full filter blur-2xl animate-blob animation-delay-6"></div>
       </div>
 
       <div className="flex flex-col items-center sm:items-start text-center sm:text-start sm:w-[35rem] my-5 mx-10 sm:m-0 space-y-4 z-30">
@@ -36,7 +36,7 @@ export default function Home() {
 
         <p className="text-base sm:text-lg text-slate-600">
           A{" "}
-          <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
+          <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent ng-clip-text">
             Fullstack Web Developer
           </span>{" "}
           based in Pemalang, Indonesia.
