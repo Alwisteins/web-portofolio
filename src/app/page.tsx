6f51c2f6352c6f4ae5739d3624e9303e032348fb
unknown <alwyyourbae@gmail.com>
2024-05-29 16:46:53 +0700

    add about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Home from "@/section/home/Home";
+import About from "@/section/about/About";
 
 export default function Page() {
     return (
-      <main className="flex min-h-screen w-full flex-col items-center justify-between">
+      <main className="flex min-h-screen w-full flex-col items-center justify-between space-y-1 bg-[#f9f9f9]">
         <Home />
+        <About />
       </main>
     );
   }
