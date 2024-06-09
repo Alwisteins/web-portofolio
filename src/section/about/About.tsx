4764777b54dc9210aa39d20940e706f203d06eae
unknown <alwyyourbae@gmail.com>
2024-06-09 19:48:07 +0700

    fix about section

--- src/section/about/About.tsx
+++ src/section/about/About.tsx
@@ -1,18 +1,18 @@
 export default function About() {
   return (
     <div className="px-10 py-10 sm:px-60 sm:h-[70vh] flex flex-col items-center justify-center">
-      <h1 className="font-bold text-center sm:text-3xl text-2xl">About me</h1>
-      <p className="text-xs text-justify sm:text-lg pt-3">
+      <h1 className="font-bold text-center sm:text-5xl text-3xl">About me</h1>
+      <p className="text-justify sm:text-lg pt-3">
         My name is Alwi wahyu waludin. A fresh graduate from SMK Muhammadiyah 01
         Pemalang as Telecommunications Network and Computer Engineering student.
         I live in Pemalang, Central Java, Indonesia.
       </p>
-      <p className="text-xs text-justify sm:text-lg pt-3">
-        I am a fullstack web developer focused on learning web development using
+      <p className="text-justify sm:text-lg pt-3">
+        I am a Fullstack Web Developer focused on learning web development using
         MongoDB, ExpressJS, ReactJS, NodeJS (MERN stack) for 2 years. During
-        that time, I have created web projects several times as a backend and
-        frontend developer and carried outvseveral project collaborations with
-        team members.
+        that time, I have created web projects several times and carried out
+        several project collaborations with team members as a Backend and
+        Frontend Developer.
       </p>
     </div>
   );
