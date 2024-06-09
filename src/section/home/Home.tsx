793ebf290c6bfe8a4f45bc0e9022a763e84a5687
unknown <alwyyourbae@gmail.com>
2024-06-09 18:31:13 +0700

    update home section

--- src/section/home/Home.tsx
+++ src/section/home/Home.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import AlwiIcon from "@/alwi-rounded-mask.jpg";
+import AlwiMainPicture from "@/alwi-django.jpeg";
 import { HiDocumentDownload } from "react-icons/hi";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { Button } from "flowbite-react";
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="flex flex-col sm:flex-row-reverse items-center justify-start py-8 sm:justify-center space-y-8 sm:space-x-12 sm:space-x-reverse w-screen h-[90vh]">
       <div className="flex w-72 h-72 sm:w-[22rem] sm:h-[22rem]">
         <Image
-          src={AlwiIcon}
+          src={AlwiMainPicture}
           alt="Alwi Main Image"
           quality={100}
           sizes="100vw"
@@ -20,7 +20,7 @@ export default function Home() {
         />
       </div>
 
-      <div className="absolute sm:top-40 left-0 top-80">
+      <div className="absolute sm:top-40 left-0 sm:left-60 top-80">
         <div className="absolute left-0 sm:-top-16 z-20 w-96 h-96 sm:w-[40rem] sm:h-[40rem] opacity-80 mix-blend-overlay">
           <Image src={Texture} alt="Gradient Texture" className="w-full" />
         </div>
