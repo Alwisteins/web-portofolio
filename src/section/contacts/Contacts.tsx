e1d5b936c3039796d98002e7b93e0065be92e137
unknown <alwyyourbae@gmail.com>
2024-06-11 09:25:41 +0700

    fix contacts description

--- src/section/contacts/Contacts.tsx
+++ src/section/contacts/Contacts.tsx
@@ -5,8 +5,7 @@ export default function Contacts() {
     <div className="px-10 py-16 sm:px-60">
       <h1 className="text-center font-bold text-3xl sm:text-5xl">React me!</h1>
       <p className="sm:text-lg text-center py-3">
-        Please don’t hesitate to get in touch with me by following my social
-        media below:
+        If you want to know more about me, feel free to email me:
       </p>
       <Form />
     </div>
