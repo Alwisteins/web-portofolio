36b45375134678c9a5bb6d2742ed4b01d6ba7c8d
unknown <alwyyourbae@gmail.com>
2024-06-11 08:40:02 +0700

    fix contacts section description

--- src/section/contacts/Contacts.tsx
+++ src/section/contacts/Contacts.tsx
@@ -1,15 +1,12 @@
-import Form from '../../components/Form'
+import Form from "../../components/Form";
 
 export default function Contacts() {
   return (
     <div className="px-10 py-16 sm:px-60">
       <h1 className="text-center font-bold text-3xl sm:text-5xl">React me!</h1>
-      <p className="sm:text-lg">
-      I am a Fullstack Web Developer focused on learning web development using
-        MongoDB, ExpressJS, ReactJS, NodeJS (MERN stack) for 2 years. During
-        that time, I have created web projects several times and carried out
-        several project collaborations with team members as a Backend and
-        Frontend Developer.
+      <p className="sm:text-lg text-center py-3">
+        Please don’t hesitate to get in touch with me by following my social
+        media below:
       </p>
       <Form />
     </div>
