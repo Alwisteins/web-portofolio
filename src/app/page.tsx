88dcf3a9100e53ef1c9b0ffa3832e19436332a3c
unknown <alwyyourbae@gmail.com>
2024-06-11 07:06:17 +0700

    add contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Home from "@/section/home/Home";
 import About from "@/section/about/About";
 import Projects from "@/section/projects/Projects";
+import Contacts from "@/section/contacts/Contacts";
 
 export default function Page() {
   return (
@@ -8,6 +9,7 @@ export default function Page() {
       <Home />
       <About />
       <Projects />
+      <Contacts />
     </main>
   );
 }
---
+++ src/section/contacts/Contacts.tsx
@@ -0,0 +1,17 @@
+import Form from '../../components/Form'
+
+export default function Contacts() {
+  return (
+    <div className="px-10 py-16 sm:px-60">
+      <h1 className="text-center font-bold text-3xl sm:text-5xl">React me!</h1>
+      <p className="sm:text-lg">
+      I am a Fullstack Web Developer focused on learning web development using
+        MongoDB, ExpressJS, ReactJS, NodeJS (MERN stack) for 2 years. During
+        that time, I have created web projects several times and carried out
+        several project collaborations with team members as a Backend and
+        Frontend Developer.
+      </p>
+      <Form />
+    </div>
+  );
+}
